c1f2ca71ba4e33edc9d76b1806469b1a4fff8358
Olya <olgariabuxa0@gmail.com>
2024-11-14 14:53:17 +0200

    add hw4.2

--- js/m4.1.js
+++ js/m4.1.js
@@ -1,24 +1,377 @@
 'use strict';
 
-const total = 5;
-const lalala = 6;
-function foo() {
-    if (total <= lalala) {
-        return true
-    }
-    return false
-}
+// const total = 5;
+// const lalala = 6;
+// function foo() {
+//     if (total <= lalala) {
+//         return true
+//     }
+//     return false
+// }
 
-console.log(foo());
+// console.log(foo());
 
+
+// const total = 5;
+// const lalala = 6;
+// function foo() {
+//     return total <= lalala
+// }
+
+// console.log(foo());
 
 
 
 // Об'єкти
+// Створення об'єкта
+// Вкладені властивості
 
+// Двокрапка між об'єктом і контентом - ключ: значення
+// Розділювач між ключами "кома";
+// У консоль показує за алфавітом
+
+// const playlist = {
+//     name: "My playlist",
+//     rating: 5,
+//     tracks: ['track-1', 'track-2', 'track-3'],
+//     isFavorite: true,
+// }
+
+// console.log(playlist.name);
+
+// можна звернутись за назвою ключа - console.log(playlist.name);
+
+
+// const playlist = {
+//     name: "My playlist",
+//     rating: 5,
+//     tracks: ['track-1', 'track-2', 'track-3'],
+//     isFavorite: true,
+// }
+
+// const prop = "rating";
+// console.log(playlist[prop]);
+
+// це якщо не знаю назву змінної;
+
+
+// const playlist = {
+//     name: "My playlist",
+//     rating: 5,
+//     tracks: ['track-1', 'track-2', 'track-3'],
+//     isFavorite: true,
+// }
+
+// playlist.name = "New playlist";
+// playlist.location = "Dnipro";
+// playlist.name = "lalala";
+// console.log(playlist);
+
+// переініціалізовую (перезаписую) значення - playlist.name = "New playlist", playlist.name = "lalala";;
+// записуємо новий ключ і його значення - playlist.location = "Dnipro";
+
+
+// const playlist = {
+//     name: "My playlist",
+//     rating: 5,
+//     tracks: ['track-1', 'track-2', 'track-3'],
+//     isFavorite: true,
+// }
+
+// const a = "location";
+// playlist[a] = "lalala";
+// console.log(playlist);
+
+// це якщо не знаю назву змінної;
+
+
+// Доступ до вкладених властивостей:
 // Доступ до властивостей через крапку
 // Доступ до вкладених властивостей
 // Доступ до властивостей через квадратні дужки
 // Зміна значення властивостей
 // Додавання вдастивостей
 
+// const obj = {
+//     name: "Alice",
+//     age: 25,
+//     skills: {
+//         html: true,
+//         css: true,
+//         js: false
+//     }
+// }
+
+// console.log(obj.skills);
+
+
+// const obj = {
+//     name: "Alice",
+//     age: 25,
+//     skills: {
+//         html: true,
+//         css: true,
+//         js: false
+//     }
+// }
+
+// const obj1 = {html: true, css: true, js: false}
+// console.log(obj1.html);
+
+
+// const obj = {
+//     name: "Alice",
+//     age: 25,
+//     skills: {
+//         html: true,
+//         css: true,
+//         js: false
+//     }
+// }
+
+// const obj1 = {html: true, css: true, js: false}
+// console.log(obj.skills.html);
+
+
+// const obj = {
+//     name: "Alice",
+//     age: 25,
+//     skills: {
+//         html: true,
+//         css: true,
+//         js: false
+//     }
+// }
+
+// obj.skills.js = true;
+// console.log(obj);
+
+
+
+// Об'єкти
+// Тип даних за посиланням
+// Масиви і функції - це об'єкти`
+
+// const arr = [1, 2, 3];
+// arr.hello = "hi"
+
+// console.log(arr);
+
+// ТАК РОБИТИ НЕ МОЖНА
+
+
+// function foo() {
+//     console.log("lalala");
+// }
+// foo.hello = "hi";
+// console.dir(foo);
+
+// ФУНКЦІЇ І МАСИВИ ЦЕ ТАКОЖ ОБ'ЄКТИ
+
+
+
+// Як копіюються масиви? - За посиланням
+
+// const a = { x: 1, y: 2 }; // http://my-computer/obj/1
+// const b = a; // http://my-computer/obj/1
+
+// console.log(a === b);
+
+// a.x = 100;
+
+// console.log("a", a);
+// console.log("b", b);
+
+
+// const a = { x: 1, y: 2 }; // http://my-computer/obj/1
+// const b = a; // http://my-computer/obj/1
+
+// const obj1 = { x: 1, y: 2 }; // http://my-computer/obj/2
+// const obj2 = { x: 1, y: 2 }; // http://my-computer/obj/3
+
+// console.log(obj1 === obj2);
+
+// Тут порівнюються два посилання
+
+
+
+// Об'єкти
+// Короткі властивості
+
+// Це кщо однакові ключі і значення
+
+// const username = "Alice";
+// const age = 30;
+
+// const user = {
+//     username,
+//     age
+// }
+
+// console.log(user);
+
+
+
+// Обчислювальні властивості
+
+// const inputName = "color";
+// const picker = {
+//     // color: "#afafaf"
+//     [inputName]: "#afafaf"
+// }
+
+// console.log(picker);
+
+
+// function creatrObj(key, value) {
+//     return {
+//         [key]: value
+//     }
+// }
+
+// console.log(creatrObj("name", "Kate"));
+// console.log(creatrObj("color", "red"));
+
+
+
+// Перебір об'єкта
+// Цикл for...in
+
+// const feedback = {
+//     good: 5,
+//     neutral: 10,
+//     bad: 3
+// }
+// let total = 0;
+// for (const key in feedback) {
+//     //const key = "good"
+//     //const key = "neutral"
+//     //const key = "bad"
+//     total += feedback[key];
+// }
+
+// console.log(total);
+
+
+
+// Метод Object.keys
+
+// const feedback = {
+//     good: 5,
+//     neutral: 10,
+//     bad: 3
+// }
+// const keys = Object.keys(feedback);
+// let total = 0;
+// for (const key of keys) {
+//    total += feedback[key];
+// }
+
+// console.log(total);
+
+
+
+// Метод Object.values
+
+// const feedback = {
+//     good: 5,
+//     neutral: 10,
+//     bad: 3
+// }
+// const values = Object.values(feedback);
+// let total = 0;
+// for (const item of values) {
+//    total += item;
+// }
+
+// console.log(total);
+
+
+
+// Напиши скрипт який для об'єкта user, послідовно:
+// - додає поле mood зі значенням "happy"
+// - замінює значення hobby на "skydiving"
+// - замінює значення premium на false
+// - виводить вміст об'єкта user у форматі ключ: значення використову
+
+// const user = {
+//     name: "Alice",
+//     age: 20,
+//     hobby: "html",
+//     premium: true
+// }
+// user.mood = "happy";
+// user.hobby = "skydiving";
+// user.premium = false;
+// const keys = Object.keys(user);
+// for (const key of keys) {
+//     console.log(`${key}: ${user[key]}`);
+// }
+
+
+// const user = {
+//     name: "Alice",
+//     age: 20,
+//     hobby: "html",
+//     premium: true
+// }
+// const user1 = {
+//     name: "Kate",
+//     age: 22,
+//     hobby: "css",
+//     premium: true
+// }
+// function foo(obj) {
+//     obj.mood = "happy";
+//     obj.hobby = "skydiving";
+//     obj.premium = false;
+//     const keys = Object.keys(obj);
+//     for (const key of keys) {
+//         console.log(`${key}: ${obj[key]}`);
+//     }
+// }
+// foo(user);
+// foo(user1);
+
+
+// const a = 5;
+// function foo(num) { // let num = 5
+//     return ++num;
+// }
+
+// console.log("foo", foo(a));
+// console.log("a", a);
+
+
+// const numbers = [1, 2, 3];
+// function foo(arr) {
+//     arr.push(4);
+//     return arr;
+// }
+
+// console.log("foo", foo(numbers));
+// console.log("numbers", numbers);
+
+// ми пішли за посиланням і там його змінили
+
+
+
+// У нас є об'єкт, де зберігаються зарплати нашої команди. Напишіть код для
+// підсумовування всіх зарбат і збережіть результат у змінній sum.
+// Повинно вийти 390. Якщо об'єкт salaries порожній, то результат має бути 0.
+
+// const salaries = {
+//     Kate: 130,
+//     Alice: 160,
+//     Petya: 100
+// }
+// function summa(obj) {
+//     let total = 0;
+//     const values = Object.values(obj);
+//     for (const value of values) {
+//         total += value;
+//     }
+//     return total;
+// }
+
+// console.log(summa(salaries));
--- js/m4.2.js
+++ js/m4.2.js
@@ -1,1 +1,317 @@
 'use strict';
+
+// const obj = {
+//     desc: "lorem",
+//     rating: 4,
+//     price: 2153
+// }
+// const keys = [];
+// const values = [];
+// for (const key in obj) {
+//     keys.push(key)
+//     values.push(obj[key]);
+// }
+
+// console.log(keys);
+// console.log("values", values);
+
+
+// function foo() {
+//     const arr = Array.from(arguments);
+//     return arr.reverse();
+// }
+
+// console.log(foo(4, 26, 23, 35, 78, 21));
+
+
+
+// Масив об'єктів
+// Перебор масива
+// Пошук об'єкта за значенням вдастивості
+// Колеція значень властивості
+
+
+
+// Пошук друга за іменем
+
+// const friends = [
+//     {name: "Ross", online: false},
+//     {name: "Joey", online: true},
+//     {name: "Chandler", online: false},
+//     {name: "Phoebe", online: true},
+//     {name: "Monica", online: true},
+//     {name: "Rachel", online: false},
+// ];
+
+// function findByName(allFriends, friendName) {
+//     for(const item of allFriends) {
+//         console.log(item);
+// }
+// }
+
+// console.log(findByName(friends, "Joey"));
+
+// *шукає всіх друзів
+
+// const friends = [
+//     {name: "Ross", online: false},
+//     {name: "Joey", online: true},
+//     {name: "Chandler", online: false},
+//     {name: "Phoebe", online: true},
+//     {name: "Monica", online: true},
+//     {name: "Rachel", online: false},
+// ];
+
+// function findByName(allFriends, friendName) {
+//     for(const item of allFriends) {
+//         console.log(item.name);
+// }
+// }
+
+// console.log(findByName(friends, "Joey"));
+
+// *шукає за ім'ям
+
+// const friends = [
+//     {name: "Ross", online: false},
+//     {name: "Joey", online: true},
+//     {name: "Chandler", online: false},
+//     {name: "Phoebe", online: true},
+//     {name: "Monica", online: true},
+//     {name: "Rachel", online: false},
+// ];
+
+// function findByName(allFriends, friendName) {
+//     for(const item of allFriends) {
+//         if (item.name.toLowerCase() === friendName.toLowerCase()) {
+//             return item;
+//         }
+//     }
+//     return "Not found";
+// }
+
+// console.log(findByName(friends, "ross"));
+
+// *шукає за іменем і нижнім та/або верхнім регістром
+// *чутливий до регістру
+
+// const friends = [
+//     {name: "Ross", online: false},
+//     {name: "Joey", online: true},
+//     {name: "Chandler", online: false},
+//     {name: "Phoebe", online: true},
+//     {name: "Monica", online: true},
+//     {name: "Rachel", online: false},
+// ];
+
+// function findByName(allFriends, friendName) {
+//     for(const item of allFriends) {
+//         if (item.name.toLowerCase() === friendName.toLowerCase()) {
+//             item.name += "!"
+//             return item;
+//         }
+//     }
+//     return "Not found";
+// }
+
+// console.log(findByName(friends, "ross"));
+
+// // *добавили "!"
+
+// const friends = [
+//     {name: "Ross", online: false},
+//     {name: "Joey", online: true},
+//     {name: "Chandler", online: false},
+//     {name: "Phoebe", online: true},
+//     {name: "Monica", online: true},
+//     {name: "Rachel", online: false},
+// ];
+
+// function findByName(allFriends, friendName) {
+//     for(const item of allFriends) {
+//         if (item.name.toLowerCase() === friendName.toLowerCase()) {
+//             item.name = "Olya"
+//             return item;
+//         }
+//     }
+//     return "Not found";
+// }
+
+// console.log(findByName(friends, "ross"));
+
+// *замінили ім'я
+
+// const friends = [
+//     {name: "Ross", online: false},
+//     {name: "Joey", online: true},
+//     {name: "Chandler", online: false},
+//     {name: "Phoebe", online: true},
+//     {name: "Monica", online: true},
+//     {name: "Rachel", online: false},
+// ];
+
+// function findByName(allFriends, friendName) {
+//     for(const item of allFriends) {
+//         if (item.name.toLowerCase() === friendName.toLowerCase()) {
+//             item.online = false;
+//             return item;
+//         }
+//     }
+//     return "Not found";
+// }
+
+// console.log(findByName(friends, "ross"));
+
+// *замінили online
+
+
+
+//Отримуємо імена всіх друзів
+
+// const friends = [
+//     {name: "Ross", online: false},
+//     {name: "Joey", online: true},
+//     {name: "Chandler", online: false},
+//     {name: "Phoebe", online: true},
+//     {name: "Monica", online: true},
+//     {name: "Rachel", online: false},
+// ];
+
+// function getAllNames(allFriends) {
+//     const result = [];
+//     for(const item of allFriends) {
+//         result.push(item.name);
+//     }
+//     return result;
+// }
+
+// console.log(getAllNames(friends));
+
+
+
+//Отримую імена тільки тих друзів, які зараз онлайн
+
+// const friends = [
+//     {name: "Ross", online: false},
+//     {name: "Joey", online: true},
+//     {name: "Chandler", online: false},
+//     {name: "Phoebe", online: true},
+//     {name: "Monica", online: true},
+//     {name: "Rachel", online: false},
+// ];
+
+// function getOnlineFriends(allFriends) {
+//     const res = [];
+//     for(const item of allFriends) {
+//        if(item.online) {
+//            res.push(item);
+//        }
+//     }
+//     return res;
+// }
+
+// console.log(getOnlineFriends(friends));
+
+// *ті хто онлайн
+
+
+// const friends = [
+//     {name: "Ross", online: false},
+//     {name: "Joey", online: true},
+//     {name: "Chandler", online: false},
+//     {name: "Phoebe", online: true},
+//     {name: "Monica", online: true},
+//     {name: "Rachel", online: false},
+// ];
+
+// function getOnlineFriends(allFriends) {
+//     const res = [];
+//     for(const item of allFriends) {
+//        if(!item.online) {
+//            res.push(item);
+//        }
+//     }
+//     return res;
+// }
+
+// console.log(getOnlineFriends(friends));
+
+// *ті хто офлайн - "!"
+
+
+
+// Напиши функцію calcTotalPrice(stones, stoneName), яка приймає
+//  масив об'єктів та рядок з назвою каменю.
+//  Функція рахує і повертає загальну вартість каміння з таким ім'ям,
+//  ціною та кількістю з об'єкта
+
+// const stones = [
+//     {name: "Смарагд", price: 1300, quantity: 4},
+//     {name: "Діамант", price: 2700, quantity: 3},
+//     {name: "Сапфір", price: 400, quantity: 7},
+//     {name: "Щебінь", price: 200, quantity: 2},
+// ]
+
+// function calcTotalPrice(arr, name) {
+//     let sum = 0;
+//     for(const stone of arr) {
+//         if(stone.name === name) {
+//             sum += stone.price * stone.quantity;
+//         }
+//     }
+//     return sum;
+// }
+
+// console.log(calcTotalPrice(stones, "Діамант"));
+
+
+
+// Об'єкти
+// Методи об'єкта
+// Доступ до властивостей об'єкта через this
+// Зміна за посиланням
+
+// *Стрілочна функція
+// const arr = () => {}
+
+// *під час виклику, this отримують з того об'єкта який викличе цю функцію
+
+// const playlist = {
+//     name: "My playlist",
+//     rating: 5,
+//     tracks: ["track-1, track-2", "track-3"],
+//     changeName(newName) { //http://foo/1
+//         console.log("this", this.name);
+//         this.name = newName;
+//     }
+// }
+
+// const petyaPlaylist = {
+//     name: "Petya playlist",
+//     rating: 4,
+//     tracks: ["track-4, track-5", "track-6"]
+// }
+
+// petyaPlaylist.changeName = playlist.changeName; //http://foo/1
+//             //http://foo/1
+// petyaPlaylist.changeName("Petya playlist");
+// playlist.changeName("lalala");
+
+// console.log("playlist", playlist);
+// console.log("petyaPlaylist", petyaPlaylist);
+
+
+const playlist = {
+    name: "My playlist",
+    rating: 5,
+    tracks: ["track-1, track-2", "track-3"],
+    changeName(newName) { //http://foo/1
+        console.log("this", this.name);
+        this.name = newName;
+    }
+}
+
+playlist.changeName("ALice playlist");
+
+console.log("playlist", playlist);
+
